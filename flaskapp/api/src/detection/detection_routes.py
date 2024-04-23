5a7bd3a96a1666cebf5a3e845840fe4bc60c153e
nafiuyakubu <naf.yak69@gmail.com>
2024-04-23 19:03:05 +0100

    fix

--- flaskapp/api/src/detection/detection_routes.py
+++ flaskapp/api/src/detection/detection_routes.py
@@ -1,14 +1,18 @@
 from flask import Blueprint, request, render_template, jsonify, abort, make_response, Response
 from PIL import Image, ImageDraw
+import cv2
 import numpy as np
 import tensorflow as tf
 import io
 import base64
+import binascii  # Import binascii module for base64 encoding
+
 
 detection_bp = Blueprint('detection', __name__)
 
+model_uri = "models/custom_model.tflite"
 # Load your trained model
-interpreter = tf.lite.Interpreter(model_path="models/custom_model.tflite")
+interpreter = tf.lite.Interpreter(model_path=model_uri)
 interpreter.allocate_tensors()
 # # Define class labels (if available)
 class_labels = ['Memory']
@@ -16,55 +20,80 @@ class_labels = ['Memory']
 input_details = interpreter.get_input_details()
 output_details = interpreter.get_output_details()
 
+# input details
+# print(input_details)
+# output details
+# print(output_details)
+
+def draw_rect2(image, box):
+    h, w = image.shape[:2]
+    y_min = int(max(1, (box[0] * h)))
+    x_min = int(max(1, (box[1] * w)))
+    y_max = int(min(h, (box[2] * h)))
+    x_max = int(min(w, (box[3] * w)))
+    
+    # draw a rectangle on the image
+    # cv2.rectangle(image, (x_min, y_min), (x_max, y_max), (255, 255, 255), 2)
+    cv2.rectangle(image, (x_min, y_min), (x_max, y_max), (0, 255, 0), 2)
+
+
 @detection_bp.route('/detect', methods=['POST'])
 def detect():
     if 'image' not in request.files:
         return jsonify({'error': 'No image uploaded'})
     
-    # image_file = request.files['image']
-    # image_bytes = image_file.read()
-    # image = Image.open(io.BytesIO(image_bytes))
-    image = Image.open(request.files['image'])
-    image = image.resize((224, 224))  # Resize to match model input size
-    image = np.array(image) / 255.0  # Normalize pixel values
-    image = np.expand_dims(image, axis=0)
 
-    # Check if the input image shape matches the model input shape
-    if image.shape != tuple(input_details[0]['shape']):
-        return jsonify({'error': 'Input image shape does not match model input shape'})
+    file = request.files['image']
+    image_bytes = file.read() # Read the uploaded image
+    nparr = np.frombuffer(image_bytes, np.uint8)
+    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)    
 
-    # Set the input tensor
-    interpreter.set_tensor(input_details[0]['index'], image.astype(np.float32))
+    # # Read the image file and preprocess it
+    # img = cv2.imdecode(np.frombuffer(image_bytes, np.uint8), -1)
+    new_img = cv2.resize(img, (448, 448))
 
-    # Run inference
+
+    interpreter.set_tensor(input_details[0]['index'], [new_img])
+
     interpreter.invoke()
 
-    # Get the output tensor
-    output_data = interpreter.get_tensor(output_details[0]['index'])
+    rect = interpreter.get_tensor(
+        output_details[0]['index'])
+    score = interpreter.get_tensor(
+        output_details[2]['index'])
 
-    # Post-process predictions (e.g., filter by confidence threshold)
-    detections = [(CLASSES[i], float(output_data[0][i])) for i in range(len(output_data[0]))]
+    print("ScoreA: ", score)
+    print("Score: ", score[0])
+    # print("Rect: ", rect[0])
+    # print("img: ", img)
 
-    return render_template('index.html', result_image=detections)
-    # return jsonify({'detections': detections})
+    if score[0] > 20.0:
+        print("Image Pass: ")
+        draw_rect2(new_img, rect[0])
+        # buffer = cv2.imencode('.jpg', new_img)
+        # print("Encoded img: ", buffer)
+        # image_base64 = base64.b64encode(buffer).decode('utf-8')
+        # Assuming `new_img` is your image data
 
-    # # Preprocess the image
-    # processed_image = preprocess_image(image)
-    # # Make predictions
-    # predictions = model.predict(np.expand_dims(processed_image, axis=0))
-    # # Process predictions and draw bounding boxes
-    # predictions = model.predict(np.expand_dims(processed_image, axis=0))
-    # # Extract bounding box coordinates and class labels from predictions
-    # boxes = predictions[:, :, :4]  # Assuming predictions are in the format (batch_size, num_boxes, 4) for box coordinates
-    # class_indices = np.argmax(predictions[:, :, 4:], axis=-1)  # Assuming predictions are in the format (batch_size, num_boxes, num_classes)
-    # # Convert class indices to class labels
-    # class_labels = [class_labels[idx] for idx in class_indices]
-    # # Draw bounding boxes on the image
-    # image_with_boxes = draw_boxes(image, boxes, class_labels)
+    retval, buffer = cv2.imencode('.jpg', new_img)
+    if retval:
+        # Encoding successful, convert the buffer to base64
+        encoded_image = base64.b64encode(buffer).decode('utf-8')
+        # print("Encoded img: ", image_base64)
+    else:
+        print("Failed to encode image")
+        # Alternatively, you can use binascii.b2a_base64() for encoding
+        # But ensure to pass only the image data (buffer), not the tuple
+        encoded_image = binascii.b2a_base64(buffer).decode('utf-8')
+        print("Encoded img: ", encoded_image)  
+        
 
-    # # Convert image to base64 string
-    # buffered = io.BytesIO()
-    # image_with_boxes.save(buffered, format="PNG")
-    # img_str = base64.b64encode(buffered.getvalue()).decode('utf-8')
 
-    # return render_template('index.html', result_image=img_str)
+    # for index, score in enumerate(scores[0]):
+    #     if score > 0.5:
+    #       draw_rect(new_img, rects[0][index])
+    #       encoded_img = cv2.imencode('.jpg', img)
+    #       image_base64 = base64.b64encode(encoded_img).decode('utf-8')
+
+    return render_template('index.html', result_image=encoded_image)
+    # return jsonify({'detections': "score"})
